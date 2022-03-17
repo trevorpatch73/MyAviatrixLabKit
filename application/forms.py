1ff72f38c04288c8aa8ff2e838157ee203b95022
trevorpatch73 <101590046+trevorpatch73@users.noreply.github.com>
2022-03-17 12:19:37 -0400

    Create forms.py

---
+++ application/forms.py
@@ -0,0 +1,11 @@
+from flask import *
+from wtforms import *
+from wtforms.validators import *
+
+
+class EnvVarForm(FlaskForm):
+    aws_key_id = StringField('AWS Key ID: ')
+    aws_key_value = StringField('AWS Key Value: ')
+    terraform_org_name = StringField('Terraform Organization Name: ')
+    terraform_api_key = StringField('Terraform API Key for Organization: ')
+    submit = SubmitField('Submit')
