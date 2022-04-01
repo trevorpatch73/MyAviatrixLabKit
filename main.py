05105dbfc9af3636ca88794549321cb809ecc182
trevorpatch73 <101590046+trevorpatch73@users.noreply.github.com>
2022-04-01 14:46:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+###############################
+### PY INSTALL REQUIREMENTS ###
+###############################
+# pip install flask
+# pip install flask-login
+# pip install flask-sqlalchemy
+# pip install flask-wtf
+
+
+# bastion@bastion-host-01:~$ export CONTENT_DIRECTORY=./AviatrixController
+# bastion@bastion-host-01:~$ export UPLOAD_FILE_NAME="./content-$(date +%s).tar.gz"
+# bastion@bastion-host-01:~$ tar -zcvf "$UPLOAD_FILE_NAME" -C "$CONTENT_DIRECTORY" .
+
 from application import create_app
 
 app = create_app()
